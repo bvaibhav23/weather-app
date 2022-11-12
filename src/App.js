0f14ec36570bdc97145e77403eb83cd4704b1980
bvaibhav23 <vaibhavbhaskar23@gmail.com>
2022-11-12 13:32:48 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,71 +1,45 @@
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css"
+// Components
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
-function App() {
-  const apiKey = "f56f24967aaf51182d1d4df628297c6d";
-  const [inputCity, setInputCity] = useState("");
-  const [data, setData] = useState({});
-  let [getsts,setsts]=useState(false);
+const API_KEY = "24ab6d9a1c05d95e99b111403727eb81";
 
-  const getWetherDetails = () => {
-    if (!inputCity) return;
-    const apiURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      inputCity +
-      "&appid=" +
-      apiKey;
-    fetch(apiURL)
-      .then((res) =>{ 
-        
-        if(!res.ok)
-       {
-        setsts(true)
-         return res.json()
-       }
-       else 
-        setsts(true);
-    
-    })
-      .then((data) => setData(data))
-      .catch((error)=>console.log("error", error));
-      console.log( data);
-      console.log(getsts)
+ function App() {
+    const [input,setInput]=useState();
+    const [data,setData]=useState();
+
+    const  handleChange = (event) => {
+    setInput( event.target.value)
+    setData();
   };
 
-  const handleChangeInput = (e) => {
-    // console.log("value", e.target.value);
-    setInputCity(e.target.value);
+  const handleClick = () => {
+    axios
+      .post(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          input +
+          "&units=metric&appid=" +
+          API_KEY
+      )
+      .then(res => {
+        setData(res.data)
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
- 
-  return (
-    <div className="container">
-      <div className="form-container">
-        <h1 className="heading">Weather App</h1>
-        <div className="input-container">
-        <label>Enter City Name</label>
-        <input type="text" value={inputCity} onChange={handleChangeInput}/>
-        <button onClick={getWetherDetails}>Search</button>
-        </div>
+  
+    return (
+      <div className="container">
+        <h1 className="header">Weather App</h1>
+        <Form onChange={handleChange} onClick={handleClick} />
+        {data && <Weather data={data} />}
       </div>
-
-      {Object.keys(data).length > 0  && (
-        <div className="weather-conatiner">
-             <h2 className="head">{getsts?"Enter correct city name":(data.name).toUpperCase()}</h2>
-            <h1 className="temp">
-              {getsts?"":(data.main.temp - 273.15).toFixed(2)}°C
-            </h1>
-            <h3 className="sky">
-              {getsts?"":(data.weather[0].description).toUpperCase()}
-            </h3>
-            <h3 className="humidity">
-              {getsts?"":`Humidity:${data.main.humidity}`}
-            </h3>
-
-        </div>
-      )}
-    </div>
-  );
+    );
+  
 }
-
-export default App;
+export default  App;
